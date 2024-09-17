4755ef0bab2dbe603868917d412acc67a91dfed2
Erin Millard <contact@erin.id.au>
2024-09-17 18:00:13 +1000

    Demonstrate failure when resolve config is not set

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -2,18 +2,6 @@ import { defineProject, defineWorkspace } from "vitest/config";
 
 export default defineWorkspace([
   defineProject({
-    resolve: {
-      alias: [
-        {
-          find: "msw/node",
-          replacement: "./dummy-msw.ts",
-        },
-        {
-          find: "@mswjs/interceptors/ClientRequest",
-          replacement: "./dummy-msw.ts",
-        },
-      ],
-    },
     test: {
       name: "chromium",
       browser: {
@@ -25,14 +13,6 @@ export default defineWorkspace([
     },
   }),
   defineProject({
-    resolve: {
-      alias: [
-        {
-          find: "msw/browser",
-          replacement: "./dummy-msw.ts",
-        },
-      ],
-    },
     test: {
       name: "happy-dom",
       environment: "happy-dom",
